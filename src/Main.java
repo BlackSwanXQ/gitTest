f09536f6406729882203b3736a2ba28245038955
Алексей Шлапаков <theblack2swan@gmail.com>
2024-11-21 22:10:06 +0300

    alex5

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world5");
-        System.out.println("hello world3");
+        System.out.println("x");
+        System.out.println("z");
+        System.out.println("q");
     }
 }
